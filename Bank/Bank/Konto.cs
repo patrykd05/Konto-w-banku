87a61ecfd8de104a51e92c4129b2ae4bfb4bba72
patrykd2115 <dendorpatryk@gmail.com>
2026-03-18 10:39:35 +0100

    Krok1 - wykonano

---
+++ Bank/Bank/Konto.cs
@@ -0,0 +1,93 @@
+﻿namespace Bank
+{
+    public class Konto
+    {
+        private string klient;
+        private decimal bilans;
+        private bool zablokowane = false;
+
+        public Konto(string klient, decimal bilansNaStart = 0)
+        {
+            this.klient = klient;
+            this.bilans = bilansNaStart;
+        }
+
+        public string Nazwa
+        {
+            get { return klient; }
+        }
+
+        public decimal Bilans
+        {
+            get { return bilans; }
+        }
+
+        public bool CzyZablokowane
+        {
+            get { return zablokowane; }
+        }
+
+        public void Wplata(decimal kwota)
+        {
+            if (zablokowane == true)
+            {
+                throw new InvalidOperationException("Nie można dokonać wpłaty na zablokowane konto.");
+            }
+            else
+            {
+                if (kwota < 0)
+                {
+                    throw new ArgumentException("Kwota wpłaty musi być dodatnia.");
+                }
+                else if (kwota == 0)
+                {
+                    throw new ArgumentException("Kwota wpłaty nie może być zerowa.");
+
+                }
+                else if (kwota > 0)
+                {
+                    bilans += kwota;
+                }
+            }
+        }
+
+        public void Wyplata(decimal kwota)
+        {
+            if (zablokowane == true)
+            {
+                throw new InvalidOperationException("Nie można dokonać wypłaty na zablokowane konto.");
+            }
+            else
+            {
+                if (kwota < 0)
+                {
+                    throw new ArgumentException("Kwota wypłaty musi być dodatnia.");
+                }
+                else if (kwota == 0)
+                {
+                    throw new ArgumentException("Kwota wypłaty nie może być zerowa.");
+
+                }
+                else if (kwota > bilans)
+                {
+                    throw new InvalidOperationException("Nie można wypłacić więcej niż aktualny bilans.");
+                }
+                else if (kwota > 0)
+                {
+                    bilans -= kwota;
+                }
+            }
+        }
+
+        public void BlokujKonto()
+        {
+            zablokowane = true;
+        }
+
+        public void OdblokujKonto()
+        {
+            zablokowane = false;
+        }
+
+    }
+}
---
+++ Bank/TestBank/Test1.cs
@@ -0,0 +1,128 @@
+﻿using Bank;
+namespace TestBank
+{
+    [TestClass]
+    public sealed class TestyKonstruktor
+    {
+        [TestMethod]
+        public void KonstruktorPoprawneDane()
+        {
+            // Arrange
+            var nazwa = "Jan";
+            decimal saldoPoczatkowe = 1000;
+
+            // Act
+            var konto = new Konto(nazwa, saldoPoczatkowe);
+
+            // Assert
+            Assert.AreEqual(nazwa, konto.Nazwa);
+            Assert.AreEqual(saldoPoczatkowe, konto.Bilans);
+        }
+    }
+
+    [TestClass]
+    public sealed class TestyWplata
+    {
+        [TestMethod]
+        public void Wplata_PoprawnaKwota()
+        {
+            var konto = new Konto("Jan", 100);
+
+            konto.Wplata(50);
+
+            Assert.AreEqual(150, konto.Bilans);
+        }
+
+        [TestMethod]
+        public void Wplata_UjemnaKwota()
+        {
+            var konto = new Konto("Jan", 100);
+
+            Assert.Throws<ArgumentException>(() => konto.Wplata(-10));
+        }
+
+        [TestMethod]
+        public void Wplata_Zero()
+        {
+            var konto = new Konto("Jan", 100);
+
+            Assert.Throws<ArgumentException>(() => konto.Wplata(0));
+        }
+
+        [TestMethod]
+        public void Wplata_ZablokowaneKonto()
+        {
+            var konto = new Konto("Jan", 100);
+            konto.BlokujKonto();
+            Assert.Throws<InvalidOperationException>(() => konto.Wplata(50));
+        }
+    }
+
+    [TestClass]
+    public sealed class TestyWyplata
+    {
+        [TestMethod]
+        public void Wyplata_PoprawnaKwota()
+        {
+            var konto = new Konto("Jan", 200);
+
+            konto.Wyplata(50);
+
+            Assert.AreEqual(150, konto.Bilans);
+        }
+
+        [TestMethod]
+        public void Wyplata_ZaDuzo()
+        {
+            var konto = new Konto("Jan", 100);
+
+            Assert.Throws<InvalidOperationException>(() => konto.Wyplata(200));
+        }
+
+        [TestMethod]
+        public void Wyplata_Zero()
+        {
+            var konto = new Konto("Jan", 100);
+
+            Assert.Throws<ArgumentException>(() => konto.Wyplata(0));
+        }
+
+        [TestMethod]
+        public void Wyplata_ZablokowaneKonto()
+        {
+            var konto = new Konto("Jan", 100);
+            konto.BlokujKonto();
+
+            Assert.Throws<InvalidOperationException>(() => konto.Wyplata(50));
+        }
+    }
+
+    [TestClass]
+    public sealed class TestyBlokowanieKonta
+    {
+        [TestMethod]
+        public void BlokujKonto()
+        {
+            var konto = new Konto("Jan");
+
+            konto.BlokujKonto();
+
+            Assert.IsTrue(konto.CzyZablokowane);
+        }
+    }
+
+    [TestClass]
+    public sealed class TestyOdblokowanieKonta
+    {
+        [TestMethod]
+        public void OdblokujKonto()
+        {
+            var konto = new Konto("Jan");
+            konto.BlokujKonto();
+
+            konto.OdblokujKonto();
+
+            Assert.IsFalse(konto.CzyZablokowane);
+        }
+    }
+}
